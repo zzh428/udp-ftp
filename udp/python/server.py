d89d6cff0b109ff25d2fe5fa32ed251d9842a2cb
zzh428 <zhaozhehui16@163.com>
2018-10-23 10:24:31 +0800

    udp finished right

--- udp/python/server.py
+++ udp/python/server.py
@@ -10,7 +10,7 @@ try:
   while True:
     data, address = sock.recvfrom(size)
     print(str(data,encoding='utf-8'))
-    sock.sendto(bytes(count) +bytes(' ',encoding='utf-8') + data, address)
+    sock.sendto(bytes(str(count),encoding='utf-8') +bytes(' ',encoding='utf-8') + data, address)
     count += 1
 finally:
   sock.close()
