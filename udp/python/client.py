ca7d2fae765092cb123db89990b79007dacfd9ef
zzh428 <624777990@qq.com>
2018-10-23 09:59:12 +0800

    Update udp/python/client.py

--- udp/python/client.py
+++ udp/python/client.py
@@ -4,7 +4,7 @@ size = 8192
  
 #try:
 for i in range(51):
-  msg = bytes(i)
+  msg = bytes(str(i,encoding='utf-8'))
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.sendto(msg, ('localhost', 9876))
   print(str(sock.recv(size), encoding = "utf-8"))
